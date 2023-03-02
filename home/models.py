f909e541b5ec0671e692d06254fb662542618015
eritten <59349542+eritten@users.noreply.github.com>
2023-03-01 21:21:13 -0800

    second commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.timezone import now
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(default=now)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images')
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', args=(self.pk, self.title))
